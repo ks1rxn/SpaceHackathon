373188b81be3acd63af79b8f0e70a044d64d7279
Eugene Lagutkin <eugene.lagutkin@gmail.com>
2016-09-13 02:39:48 +0300

    camera work

--- SpaceHacathon/Assets/Scripts/BattleCamera.cs
+++ SpaceHacathon/Assets/Scripts/BattleCamera.cs
@@ -4,11 +4,11 @@ public class BattleCamera : MonoBehaviour {
 
 	protected void Update() {
 		Vector3 lookVector = new Vector3(Mathf.Cos(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180));
-		Vector3 position = BattleContext.PlayerShip.transform.position - lookVector * 5f;
-		position.y = 7.5f;
+		Vector3 position = BattleContext.PlayerShip.transform.position - lookVector * 7f;
+		position.y = 10.5f;
 		transform.position = position;
 		Vector3 rotation = BattleContext.PlayerShip.transform.eulerAngles;
-		rotation.x = 60;
+		rotation.x = 90;
 		rotation.y += 90;
 		transform.eulerAngles = rotation;
 	}
