944fb83afb3de9b50652189f6a47f879b3f5678b
Eugene Lagutkin <eugene.lagutkin@gmail.com>
2016-09-13 01:51:49 +0300

    Camera behind

--- SpaceHacathon/Assets/Scripts/BattleCamera.cs
+++ SpaceHacathon/Assets/Scripts/BattleCamera.cs
@@ -3,9 +3,14 @@
 public class BattleCamera : MonoBehaviour {
 
 	protected void Update() {
-		Vector3 position = BattleContext.PlayerShip.transform.position;;
-		position.y = 15;
+		Vector3 lookVector = new Vector3(Mathf.Cos(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180));
+		Vector3 position = BattleContext.PlayerShip.transform.position - lookVector * 5f;
+		position.y = 7.5f;
 		transform.position = position;
+		Vector3 rotation = BattleContext.PlayerShip.transform.eulerAngles;
+		rotation.x = 60;
+		rotation.y += 90;
+		transform.eulerAngles = rotation;
 	}
 
 }
