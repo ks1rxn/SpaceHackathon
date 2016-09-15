b0871d364f899dd2a6460bfd6a6104c8bffafe83
Eugene Lagutkin <eugene.lagutkin@gmail.com>
2016-09-15 21:00:51 +0300

    temp player ship

--- SpaceHacathon/Assets/Scripts/BattleCamera.cs
+++ SpaceHacathon/Assets/Scripts/BattleCamera.cs
@@ -1,69 +1,23 @@
 ﻿using UnityEngine;
 
 public class BattleCamera : MonoBehaviour {
-	private int mode = 0;
-
 	protected void Awake() {
 		BattleContext.BattleCamera = this;
 	}
 
 	protected void Update() {
-		if (Input.GetKeyDown(KeyCode.Alpha1)) {
-			mode = 0;
-		} else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-			mode = 1;
-		} else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-			mode = 2;
-		}
-		switch (mode) {
-			case 0:
-				CameraStatic();
-				break;
-			case 1:
-				CameraBehind();
-				break;
-			case 2:
-				CameraBehindWide();
-				break;
-		}
-	}
-
-	private void CameraBehind() {
-		Vector3 lookVector = new Vector3(Mathf.Cos(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180));
-		Vector3 position = BattleContext.PlayerShip.transform.position - lookVector * 7f;
-		position.y = 10.5f;
-		transform.position = position;
-		Vector3 rotation = BattleContext.PlayerShip.transform.eulerAngles;
-		rotation.x = 50;
-		rotation.y += 90;
-		transform.eulerAngles = rotation;
-		GetComponent<Camera>().fieldOfView = 60;
-	}
-
-	private void CameraBehindWide() {
-		Vector3 lookVector = new Vector3(Mathf.Cos(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-BattleContext.PlayerShip.transform.rotation.eulerAngles.y * Mathf.PI / 180));
-		Vector3 position = BattleContext.PlayerShip.transform.position - lookVector * 5f;
-		position.y = 7.5f;
-		transform.position = position;
-		Vector3 rotation = BattleContext.PlayerShip.transform.eulerAngles;
-		rotation.x = 50;
-		rotation.y += 90;
-		transform.eulerAngles = rotation;
-		GetComponent<Camera>().fieldOfView = 75;
-	}
-
-	private void CameraStatic() {
 		Vector3 position = BattleContext.PlayerShip.transform.position;
-		position.y = 7.5f;
-		position.z -= 7;
+		position.y = 2.5f;
+		position.z -= 2;
 		transform.position = position;
 		transform.eulerAngles = new Vector3(50, 0, 0);
 		GetComponent<Camera>().fieldOfView = 75;
+//		Vector3 position = BattleContext.PlayerShip.transform.position;
+//		position.y = 7.5f;
+//		position.z -= 7;
+//		transform.position = position;
+//		transform.eulerAngles = new Vector3(50, 0, 0);
+//		GetComponent<Camera>().fieldOfView = 75;
 	}
 
-	public int Mode {
-		get {
-			return mode;
-		}
-	}
 }
--- SpaceHacathon/Assets/Scripts/PlayerShip.cs
+++ SpaceHacathon/Assets/Scripts/PlayerShip.cs
@@ -21,12 +21,6 @@ public class PlayerShip : MonoBehaviour {
 	}
 
 	protected void Update() {
-//		if (BattleContext.BattleCamera.Mode == 0) {
-//			MoveForStatic();
-//		} else {
-//			MoveForBehind();
-//		}
-
 		Move();
 
 		// Trash //
@@ -39,11 +33,6 @@ public class PlayerShip : MonoBehaviour {
 
 		if (Input.GetKeyDown(KeyCode.Alpha5)) {
 			m_slow = !m_slow;
-//			 if (Time.timeScale == 1.0F)
-//                Time.timeScale = 0.3F;
-//            else
-//                Time.timeScale = 1.0F;
-//            Time.fixedDeltaTime = 0.02F * Time.timeScale;
 		}
 		if (m_slow && Time.timeScale > 0.3f) {
 			Time.timeScale -= Time.deltaTime * 5;
@@ -73,65 +62,6 @@ public class PlayerShip : MonoBehaviour {
 		}
 		if (m_rigidbody.velocity.magnitude > 5) {
 			m_rigidbody.velocity = m_rigidbody.velocity.normalized * 5;
-		}
-	}
-
-	private void MoveForBehind() {
-		Vector3 lookVector = new Vector3(Mathf.Cos(-transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-transform.rotation.eulerAngles.y * Mathf.PI / 180));
-		bool leftEngine = false;
-		bool rightEngine = false;
-		if (Input.GetKey(KeyCode.D)) {
-			leftEngine = true;
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			rightEngine = true;
-		}
-		if ((leftEngine) && (!rightEngine)) {
-			if (m_rigidbody.angularVelocity.magnitude < 0.75) {
-				m_rigidbody.AddTorque(new Vector3(0, 3000, 0));
-			}
-//			m_rigidbody.AddForce(lookVector);
-		}
-		if ((!leftEngine) && (rightEngine)) {
-//			m_rigidbody.AddForce(lookVector);
-			if (m_rigidbody.angularVelocity.magnitude < 0.75) {
-				m_rigidbody.AddTorque(new Vector3(0, -3000, 0));
-			}
-		}
-		if ((leftEngine) && (rightEngine)) {
-			m_rigidbody.AddForce(lookVector * 6);
-		}
-		if (m_rigidbody.velocity.magnitude > 5) {
-			m_rigidbody.velocity = m_rigidbody.velocity.normalized * 5;
-		}
-	}
-
-	private void MoveForStatic() {
-		Vector3 lookVector = new Vector3(Mathf.Cos(-transform.rotation.eulerAngles.y * Mathf.PI / 180), 0, Mathf.Sin(-transform.rotation.eulerAngles.y * Mathf.PI / 180));
-		Debug.DrawRay(transform.position, lookVector * 10, Color.red);
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			m_rigidbody.AddExplosionForce(100, transform.position - lookVector.normalized * 10, 50);
-		} else if (Input.GetMouseButton(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("InputPlane"))) {
-				float angle = MathHelper.AngleBetweenVectors(lookVector, hit.point - transform.position);
-	            if (Mathf.Abs(angle) < 90) {
-		            if ((hit.point - transform.position).magnitude > 0.5f) {
-			            float length = Mathf.Min((hit.point - transform.position).magnitude, 6);
-						m_rigidbody.AddForce(lookVector.normalized * length * 2);
-		            }
-	            }
-				m_rigidbody.AddTorque(new Vector3(0, (angle - m_rigidbody.angularVelocity.y * 50) * 75, 0));
-	            if (m_rigidbody.angularVelocity.magnitude > 2.8f) {
-		            m_rigidbody.angularVelocity = m_rigidbody.angularVelocity.normalized * 2.8f;
-	            }
-            }
-
-			if (m_rigidbody.velocity.magnitude > 5) {
-				m_rigidbody.velocity = m_rigidbody.velocity.normalized * 5;
-			}
 		}
 	}
 
