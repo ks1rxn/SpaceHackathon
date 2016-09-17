6b289a147845c663d1664e4260e79fc7cb46dc0f
Eugene Lagutkin <eugene.lagutkin@gmail.com>
2016-09-18 01:51:50 +0300

    some minor enchancements

--- SpaceHacathon/Assets/Scripts/BattleCamera.cs
+++ SpaceHacathon/Assets/Scripts/BattleCamera.cs
@@ -7,11 +7,10 @@ public class BattleCamera : MonoBehaviour {
 
 	protected void Update() {
 		Vector3 position = BattleContext.PlayerShip.transform.position;
-		position.y = 6.5f;
-		position.z -= 6;
+		position.y = 7.5f;
+		position.z -= 9;
 		transform.position = position;
 		transform.eulerAngles = new Vector3(50, 0, 0);
-		GetComponent<Camera>().fieldOfView = 75;
 	}
 
 }
