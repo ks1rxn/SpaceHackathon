f2b3a56d3bfa2a4ac807a7124caf277d2286c8f5
eugene.lagutkin <eugene.lagutkin@gmail.com>
2016-12-19 03:22:09 +0300

    charge indicators 5 charges for full stack

--- SpaceHacathon/Assets/Scripts/World/Dynamic/PlayerShip/PlayerShipChargeSystem.cs
+++ SpaceHacathon/Assets/Scripts/World/Dynamic/PlayerShip/PlayerShipChargeSystem.cs
@@ -10,11 +10,14 @@ public class PlayerShipChargeSystem : MonoBehaviour {
 	private GameObject[] m_chargeEnginesAsObjects;
 	[SerializeField]
 	private GameObject m_chargeEffect;
+	[SerializeField]
+	private GameObject[] m_chargeIndicators;
 
     private int m_chargeFuel;
 
     public void Initiate() {
 	    m_chargeFuel = 0;
+		UpdateChargeIndicators();
 		m_chargeIndicator.SetActive(false);
 
 	    foreach (ParticleSystem engine in m_chargeEngines) {
@@ -29,6 +32,7 @@ public class PlayerShipChargeSystem : MonoBehaviour {
 	public void AddFuel() {
 		if (m_chargeFuel < 5) {
 			m_chargeFuel++;
+			UpdateChargeIndicators();
 			if (InChargeTargeting) {
 				m_chargeIndicator.SetActive(true);
 				foreach (ParticleSystem engine in m_chargeEngines) {
@@ -41,8 +45,20 @@ public class PlayerShipChargeSystem : MonoBehaviour {
 		}
 	}
 
+	private void UpdateChargeIndicators() {
+		m_chargeIndicators[0].SetActive(m_chargeFuel >= 1);
+		m_chargeIndicators[1].SetActive(m_chargeFuel >= 2);
+		m_chargeIndicators[2].SetActive(m_chargeFuel >= 3);
+		m_chargeIndicators[3].SetActive(m_chargeFuel >= 4);
+		m_chargeIndicators[4].SetActive(m_chargeFuel >= 5);
+	}
+
 	public void Charge() {
-		m_chargeFuel = 0;
+		if (!InChargeTargeting) {
+			return;
+		}
+		m_chargeFuel -= 1;
+		UpdateChargeIndicators();
 		m_chargeIndicator.SetActive(false);
 		foreach (ParticleSystem engine in m_chargeEngines) {
 			engine.Stop();
@@ -112,7 +128,7 @@ public class PlayerShipChargeSystem : MonoBehaviour {
 
 	public bool InChargeTargeting {
 		get {
-			return m_chargeFuel == 5;
+			return m_chargeFuel > 0;
 		}
 	}
 
