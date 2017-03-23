5409d6e74a838a63e618397337304d20795ac054
eugene.lagutkin <eugene.lagutkin@gmail.com>
2017-03-23 21:10:58 +0300

    HealDroneStation functionality

--- SpaceHacathon/Assets/Scripts/Tools/Settings/Data/Allies/SettingsHealingDroneStation.cs
+++ SpaceHacathon/Assets/Scripts/Tools/Settings/Data/Allies/SettingsHealingDroneStation.cs
@@ -1,4 +1,7 @@
 ﻿
 public class SettingsHealingDroneStation {
 	public float HealingRadius;
+	public float DistanceFromPlayerToDespawn;
+	public float TotalHealthCapacity;
+	public float TimeToGiveTotalHealth;
 }
