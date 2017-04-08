19152981ad145025830bf42966077a9eee2ba606
eugene.lagutkin <eugene.lagutkin@gmail.com>
2017-04-08 23:36:23 +0300

    explosions refactoring

--- SpaceHacathon/Assets/Scripts/World/Dynamic/Explosions/ShipExplosion.cs
+++ SpaceHacathon/Assets/Scripts/World/Dynamic/Explosions/ShipExplosion.cs
@@ -1,42 +1,5 @@
 ﻿using UnityEngine;
 
 public class ShipExplosion : IExplosion {
-	[SerializeField]
-	private float m_lifeTime;
-	[SerializeField]
-	private ParticleSystem[] m_particles;
-
-    private float m_timeToDie;
-
-	protected override void OnInitiate() {
-	}
-
-	protected override void OnSpawn(Vector3 position, Vector3 angle) {
-		foreach(ParticleSystem system in m_particles) {
-            system.Play();
-        }
-        
-		m_timeToDie = m_lifeTime;
-	}
-
-	protected override void OnDespawn(DespawnReason reason) {
-
-	}
-
-	protected override void OnFixedUpdateEntity() {
-		m_timeToDie -= Time.deltaTime;
-		if (m_timeToDie <= 0) {
-			foreach(ParticleSystem system in m_particles) {
-				system.Stop();
-			}
-			Despawn(DespawnReason.TimeOff);
-		}
-	}
-
-	protected override float DistanceToDespawn {
-		get {
-			return 30;
-		}
-	}
 
 }
