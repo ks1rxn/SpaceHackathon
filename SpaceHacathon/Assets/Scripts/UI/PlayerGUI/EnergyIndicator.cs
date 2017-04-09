4a7f7e4e158039aeffe27185aae7cd4d370b5d99
Eugene Lagutkin <eugene.lagutkin@gmail.com>
2017-04-09 17:08:58 +0300

    energy indicator improvement

--- SpaceHacathon/Assets/Scripts/UI/PlayerGUI/EnergyIndicator.cs
+++ SpaceHacathon/Assets/Scripts/UI/PlayerGUI/EnergyIndicator.cs
@@ -10,17 +10,22 @@ public class EnergyIndicator : MonoBehaviour {
 	public void SetEnergy(float energy) {
 		int energyInt = Mathf.RoundToInt(energy * 100);
 		int div = energyInt / 20;
-		float ost = (energyInt % 20) / 20.0f;
-//		Debug.Log(energyInt + " " + div + " " + ost);
+		float ost = (int)((energyInt % 20) / 20.0f * 5) * 0.2f;
+		float ost2 = ((int)((energyInt % 20) / 20.0f * 5) + 1) * 0.2f;
+		Debug.Log(energyInt + " " + div + " " + ost);
 		for (int i = 0; i < 5; i++) {
 			if (i > div) {
 				m_mainColors[4 - i].gameObject.SetActive(false);
+				m_blinkColors[4 - i].gameObject.SetActive(false);
 			} else {
 				m_mainColors[4 - i].gameObject.SetActive(true);
 				if (i == div) {
 					m_mainColors[4 - i].fillAmount = ost;
+					m_blinkColors[4 - i].gameObject.SetActive(true);
+					m_blinkColors[4 - i].fillAmount = ost2;
 				} else {
 					m_mainColors[4 - i].fillAmount = 1.0f;
+					m_blinkColors[4 - i].gameObject.SetActive(false);
 				}
 			}
 		}
