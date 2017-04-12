d4ca85d95e7554fafc23e51c5bc9da3b69fc51dc
eugene.lagutkin <eugene.lagutkin@gmail.com>
2017-04-12 19:46:02 +0300

    fix cargo indicator

--- SpaceHacathon/Assets/Scripts/UI/PlayerGUI/CargoIndicator.cs
+++ SpaceHacathon/Assets/Scripts/UI/PlayerGUI/CargoIndicator.cs
@@ -5,18 +5,16 @@ public class CargoIndicator : MonoBehaviour {
 	[SerializeField]
 	private Image m_top, m_middle, m_bottom;
 
-	private const float m_topW = 0.142f, m_midW = 0.13f, m_botW = 0.19f;
-
 	public void SetCargoFill(int fill) {
 		int full = fill / 3;
 		int drob = fill % 3;
 
 		Vector2 size = m_top.rectTransform.sizeDelta;
-		size.x = 58 * (full + (drob > 0 ? 1 : 0));
+		size.x = 58 * (full + (drob > 0 && fill < 25 ? 1 : 0));
 		m_top.rectTransform.sizeDelta = size;
 
 		size = m_middle.rectTransform.sizeDelta;
-		size.x = 58 * (full + (drob > 1 ? 1 : 0));
+		size.x = 58 * (full + (drob > 1 || fill == 25 ? 1 : 0));
 		m_middle.rectTransform.sizeDelta = size;
 
 		size = m_bottom.rectTransform.sizeDelta;
